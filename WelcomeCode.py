d86f5831456a97e29e33bffdb672bd1df10d4a48
NehanthWorld <NehanthWorld@NEHANTHWORLD>
2025-03-14 09:25:14 +0530

    Initial commit

---
+++ WelcomeCode.py
@@ -0,0 +1,10 @@
+print("Welcome to python")
+print("Python is awesome")
+
+
+"""this is multiline string declaration also multiline comment
+this is second line
+this is 3rd line"""
+age = "100"
+
+print(type(age))
